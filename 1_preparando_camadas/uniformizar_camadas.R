ddd874f63b038f18e85e72ee380ca9294819294f
coespicmbio <33932683+coespicmbio@users.noreply.github.com>
2017-11-23 12:21:25 -0200

    Update and rename uniformizar_camadas_Cer.R to uniformizar_camadas.R

--- 1_preparando_camadas/uniformizar_camadas.R
+++ 1_preparando_camadas/uniformizar_camadas.R
@@ -1,11 +1,12 @@
-
-#### Cortar as camadas pela interseÁ„o da camada comum ####
+###########################################################
+#### Cortar as camadas pela interse√ß√£o da camada comum ####
+###########################################################
 
 library(rgeos)
 library (raster)
 setwd("D:/PRIM_estrada/_analise_priorizacao")
 
-# Raster de m·scara do bioma (limite) (modelo)
+# Raster de m√°scara do bioma (limite) (modelo)
 cer_lim <- raster("cerrado/unidade_planejamento/raster_up_cerrado.tif")
 crs(cer_lim) <- crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
@@ -15,8 +16,6 @@ cer_lim[cer_lim==0] <- NA
 
 # Salva o raster da UP sem 0 
 writeRaster(cer_lim,"cerrado/unidade_planejamento/raster_up_cerrado.tif", NAflag= -9999, overwrite=T)
-
-
 
 # Raster para ponto
 # Gera o arquivo de pontos
@@ -28,7 +27,7 @@ crs(cer_pto) <- crs("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0
 # Troca valores NA's por zero
 #r[is.na(r[])] <- 0 
 
-# AmazÙnia
+# Amaz√¥nia
 #ameaca <- list.files("./amazonia/alvos_conservacao/ameacas", pattern = "\\.tif$", full.names = T)
 #fauna <- list.files("./amazonia/alvos_conservacao/especies_fauna", pattern = "\\.tif$", full.names = T)
 #flora <- list.files("./amazonia/alvos_conservacao/especies_flora", pattern = "\\.tif$", full.names = T)
@@ -82,11 +81,11 @@ for (rf in camadas){
   # Faz o mask
   #r_r <- mask(r_r,ca_lim)
   
-  # Iguala ‡ extens„o do raster modelo
+  # Iguala √† extens√£o do raster modelo
   #r <- extend(r,am_lim, value=0)
   #origin(r) <- origin(am_lim)
   
-  # InterseÁ„o
+  # Interse√ß√£o
   #r_int <- r_r*am_lim
   
   #plot(am_lim)
@@ -94,7 +93,7 @@ for (rf in camadas){
   
   cat ("> salvando...", "\n")
   
-  # Salva o raster com extens„o ajustada
+  # Salva o raster com extens√£o ajustada
   writeRaster(r_r,rf, NAflag= -9999, overwrite=T)
   
   # Progresso

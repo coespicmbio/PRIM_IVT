7fe3b68adfca7c485615b5976c1dffb96013dc1c
coespicmbio <33932683+coespicmbio@users.noreply.github.com>
2018-01-31 11:28:32 -0200

    Update and rename check_raster_alvos.R to confere_raster_alvos.R

--- 1_preparando_camadas/confere_raster_alvos.R
+++ 1_preparando_camadas/confere_raster_alvos.R
@@ -1,30 +1,34 @@
-#### Checar se os alvos n„o s„o vazios e est„o todos disponÌveis para o Zonation ####
+##########################################################################################################################
+#### Confere se os rasters dos alvos n√£o s√£o vazios e est√£o corretamente alocados conforme o Feature List do Zonation ####
+##########################################################################################################################
 
 library(raster)
 
-# Definir diretÛrio de trabalho
+# Define diret√≥rio de trabalho
 setwd("D:/PRIM_estrada/_analise_priorizacao/mata_atlantica")
 
 # Carrega o raster de unidades de planejamento
 up <- raster("./unidade_planejamento/raster_up_mata.tif")
 
-# Carregar o feature list
+# Carrega o feature list
 flist <- read.table(file= "./inputs/feature_list_18_01.txt", header = F, sep = "\t")
 flist <- as.vector(flist[,6])
 flist <- gsub(pattern = "mata_atlantica/",replacement = "",flist)
 
 
-# Cria a tabela vazia que receber· os dados do check
+# Cria a tabela vazia que receber√° os dados da confer√™ncia
 tab_check <- data.frame("alvo"=character(0),"existencia"=logical(0),"extensao"= logical(0),
                         "n_colunas"= logical(0), "n_linhas"= logical(0),"resolucao"= logical(0),
                         "valores"= logical(0))
 #f<- flist[1]
-# 
+
+# Realiza a confer√™ncia
+# Progresso do for loop
 p <- 1
 inicio <- date ()
 for (f in flist){
   
-  # Nome alvo
+  # Nome do alvo
   nome <- basename(f)
   nome <- gsub(pattern = "\\.tif$",replacement = "",nome)
   
@@ -35,13 +39,13 @@ for (f in flist){
   # Carrega o raster
   r <- raster(f)
   
-  # Confere se up e raster possuem os mesmos par‚metros
+  # Confere se up e raster possuem os mesmos par√¢metros
   extensao <- r@extent == up@extent
   colunas <- r@ncols == up@ncols
   linhas <- r@nrows == up@nrows
   resolucao <- TRUE %in% as.vector(res(r) == res(up))
   
-  # Confere se o raster n„o est· vazio
+  # Confere se o raster n√£o est√° vazio
   valores <- cellStats(r,stat="sum",na.rm=T)>0
   }# Fecha Condicional de existencia do raster
   if (existe==FALSE){
@@ -50,25 +54,25 @@ for (f in flist){
   linhas <- FALSE
   resolucao <- FALSE
   valores <- FALSE
-  }# Fecha Condicional de n„o existencia do raster
+  }# Fecha Condicional de n√£o existencia do raster
   
-  # Cria a tabela check do raster
+  # Cria a tabela de confer√™ncia do alvo
   tab_r <- data.frame("alvo"= nome,"existencia"=existe,"extensao"= extensao,
                       "n_colunas"= colunas, "n_linhas"= linhas,"resolucao"= resolucao,
                       "valores"= valores)
   
-  # Adiciona ‡ tabela final
+  # Adiciona √† tabela final
   tab_check <- rbind(tab_check,tab_r)
   
-  # Progresso
-  cat("Progresso",p,"de", length(flist),"<==>",round((p/length(flist)*100),2),"%","__ InÌcio:",inicio,"__ Atual:", date() ,"\n")
+  # Progresso do for loop
+  cat("Progresso",p,"de", length(flist),"<==>",round((p/length(flist)*100),2),"%","__ In√≠cio:",inicio,"__ Atual:", date() ,"\n")
   
   # Progresso
   p <- p + 1
   
 } # Fecha For Loop
 
-# Salva a tabela final
+# Salva a tabela de confer√™ncia de todos os alvos
 write.table(tab_check,file="./tab_check_alvos.txt",quote=F,sep="\t",row.names = F,overwrite=T)
 
 
